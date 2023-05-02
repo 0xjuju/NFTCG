95b12ada3465b5242751b9ce53b3a0c12dab5c12
Jermol Jupiter <jermol.jupiter@gmail.com>
2023-05-02 19:09:22 -0400

    config game rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,5 @@
+
+mod enums;
 mod template;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod structs;
+mod traits;
